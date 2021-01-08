f6cf7154ad9255679ace2bca8792715752e8346b
jessicaidro <jessicaidro@gmail.com>
2021-01-08 17:49:07 -0300

    Componente Início - Parte 1

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -10,20 +10,24 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card'; 
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
